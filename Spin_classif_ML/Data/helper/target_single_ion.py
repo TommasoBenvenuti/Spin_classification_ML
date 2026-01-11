894a2875d88dbcb26b2ee6bf95fec900576a0f43
Tommaso Benvenuti <Tommi.benvenuti5@gmail.com>
2026-01-11 18:39:19 +0100

    Delete Spin_classif_ML directory

--- Spin_classif_ML/Data/helper/target_single_ion.py
+++
@@ -1,8 +0,0 @@
-def target_single_ion(target_spin_values, target_coord_no, target_metal_z ):
-
-    collected = {s: 0 for s in target_spin_values}
-    print("Target spin values:", target_spin_values)
-    print("Target coordination number:", target_coord_no)
-    print("Target metal atomic number (Z):", target_metal_z)
-    
-    return target_spin_values, target_coord_no, collected, target_metal_z
